9b6d5e29cd0586dd801b3e5eeb647243b6aae305
Subha Patra <56377135+subha-wp@users.noreply.github.com>
2025-03-03 09:27:26 +0530

    Update top-nav.tsx

--- components/top-nav.tsx
+++ components/top-nav.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Topnav() {
   return (
-    <div className="fixed top-0 z-50 left-0 right-0 flex justify-between border-b items-center h-12 p-4">
+    <div className="fixed top-0 z-50 left-0 right-0 flex justify-between bg-purple-50 border-b items-center h-12 p-4">
       <h2 className="font-bold text-2xl tracking-wide">
         <span className="text-purple-600">Bill</span>
         <span>Bucks</span>
