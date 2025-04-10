48443a8f523445a58f485f2a21495d565055603f
Subha Patra <56377135+subha-wp@users.noreply.github.com>
2025-04-10 16:28:16 +0530

    Create page.tsx

---
+++ app/play-console/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function PlayConsolePage() {
+  redirect("https://play.google.com/store/apps/details?id=com.devcodersubha.billbucks");
+}
